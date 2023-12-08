d19d72a8687de8eef09f15aab9f0e007c2e8ed58
Steven Carmichael <blekenbleu@gmail.com>
2023-12-08 09:19:21 -0500

    missed a 'Demo'

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace XtraMouse.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("XtraMouseDemo.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("XtraMouse.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
